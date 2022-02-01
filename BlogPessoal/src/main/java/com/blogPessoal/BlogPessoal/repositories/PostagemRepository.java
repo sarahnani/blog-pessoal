a970e226b482dc93792b1b5c7e01d3e9c73d6677
Sarah Nani <sarahlenani@gmail.com>
2022-02-01 11:03:09 -0300

    Adicionando PostagemRepository

---
+++ BlogPessoal/src/main/java/com/blogPessoal/BlogPessoal/repositories/PostagemRepository.java
@@ -0,0 +1,15 @@
+package com.blogPessoal.BlogPessoal.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.blogPessoal.BlogPessoal.models.Postagem;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	
+	public List<Postagem> findAll ();
+	
+	public List<Postagem> getByTituloContainingIgnoreCase (String titulo);
+	
+}
