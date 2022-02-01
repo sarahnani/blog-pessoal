e22523536f2ba3e5c3862206a6c6c5c36b290c32
Sarah Nani <sarahlenani@gmail.com>
2022-02-01 11:25:36 -0300

    Adicionando métodos PUT, POST e DELETE no PostagemController

--- BlogPessoal/src/main/java/com/blogPessoal/BlogPessoal/controllers/PostagemController.java
+++ BlogPessoal/src/main/java/com/blogPessoal/BlogPessoal/controllers/PostagemController.java
@@ -3,10 +3,15 @@ package com.blogPessoal.BlogPessoal.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +41,23 @@ public class PostagemController {
 	@GetMapping("/titulo/{titulo}")
 	public ResponseEntity<List<Postagem>> getByTitulo (@PathVariable String titulo) {
 		return ResponseEntity.ok(repository.getByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity <Postagem> postPostagem (@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(repository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity <Postagem> putPostagem (@RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(repository.save(postagem));
+	}
+	
+	@DeleteMapping ("/{id}")
+	public void deletePostagem (@PathVariable long id) {
+		repository.deleteById(id);
 	}
 	
 }
