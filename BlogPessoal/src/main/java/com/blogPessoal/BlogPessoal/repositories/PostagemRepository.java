5bb1a666d23cb33bcfdf025740be166332532d74
Sarah Nani <sarahlenani@gmail.com>
2022-02-06 21:34:20 -0300

    Modificando repositories (security e service)

--- BlogPessoal/src/main/java/com/blogPessoal/BlogPessoal/repositories/PostagemRepository.java
+++ BlogPessoal/src/main/java/com/blogPessoal/BlogPessoal/repositories/PostagemRepository.java
@@ -3,9 +3,11 @@ package com.blogPessoal.BlogPessoal.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.blogPessoal.BlogPessoal.models.Postagem;
 
+@Repository
 public interface PostagemRepository extends JpaRepository <Postagem, Long> {
 	
 	public List <Postagem> findAll();
--- BlogPessoal/src/main/java/com/blogPessoal/BlogPessoal/repositories/TemaRepository.java
+++ BlogPessoal/src/main/java/com/blogPessoal/BlogPessoal/repositories/TemaRepository.java
@@ -3,9 +3,11 @@ package com.blogPessoal.BlogPessoal.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.blogPessoal.BlogPessoal.models.Tema;
 
+@Repository
 public interface TemaRepository extends JpaRepository <Tema, Long> {
 
 	public List <Tema> findAll();
